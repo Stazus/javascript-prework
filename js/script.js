2f7a73d2232e7ed87f22acc5a9fcf08517b8adfc
Stanisław Flak <stazus@interia.pl>
2024-11-26 18:47:43 +0100

    JS dodanie ruchów: papier i nożyce

--- js/script.js
+++ js/script.js
@@ -8,11 +8,22 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
-var computerMove;
-computerMove = "kamień";
-printMessage("Zagrałem " + computerMove + "! Jeśli Twój ruch to papier, to wygrywasz!");
+var computerMove, randomNumber;
 
-var playerMove;
-playerMove = "papier";
-printMessage("Zagrałem " + playerMove + "! Jeśli Twój ruch to nożyce, to wygrywasz!");
+// Wylosowanie liczby od 1 do 3
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('Wylosowana liczba to: ' + randomNumber);
 
+// Przypisanie ruchu komputera na podstawie wylosowanej liczby
+if (randomNumber === 1) {
+  computerMove = 'kamień';
+} else if (randomNumber === 2) {
+  computerMove = 'papier';
+} else if (randomNumber === 3) {
+  computerMove = 'nożyce';
+} else {
+  computerMove = 'nieznany ruch'; 
+}
+
+// Wyświetlenie ruchu komputera
+printMessage('Mój ruch: ' + computerMove);
