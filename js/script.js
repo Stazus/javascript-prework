b76434f12831b8410bd09cb6ff9e5af3347e88b9
Stanisław Flak <stazus@interia.pl>
2024-12-03 00:31:57 +0100

    Ostylowanie gry

--- js/script.js
+++ js/script.js
@@ -6,77 +6,77 @@ var playerMove, computerMove, randomNumber;
  * Funkcja obsługująca kliknięcie guzika
  */
 function buttonClicked(argButtonName) {
-  clearMessages();
-  console.log(argButtonName + ' został kliknięty');
+    clearMessages();
+    console.log(argButtonName + ' został kliknięty');
 
-  // Pobranie ruchu gracza na podstawie klikniętego przycisku
-  playerMove = argButtonName;
-  console.log('Ruch gracza to: ' + playerMove);
+    // Pobranie ruchu gracza na podstawie klikniętego przycisku
+    playerMove = argButtonName;
+    console.log('Ruch gracza to: ' + playerMove);
 
-  // Wylosowanie ruchu komputera
-  randomNumber = Math.floor(Math.random() * 3 + 1);
-  console.log('Wylosowana liczba to: ' + randomNumber);
+    // Wylosowanie ruchu komputera
+    randomNumber = Math.floor(Math.random() * 3 + 1);
+    console.log('Wylosowana liczba to: ' + randomNumber);
 
-  // Przypisanie ruchu komputera na podstawie losowania
-  computerMove = getMoveName(randomNumber);
-  console.log('Ruch komputera to: ' + computerMove);
+    // Przypisanie ruchu komputera na podstawie losowania
+    computerMove = getMoveName(randomNumber);
+    console.log('Ruch komputera to: ' + computerMove);
 
-  // Wyświetlenie wyniku gry
-  displayResult(playerMove, computerMove);
+    // Wyświetlenie wyniku gry
+    displayResult(playerMove, computerMove);
 }
 
 /**
  * Funkcja wyświetlająca wiadomości na ekranie
  */
 function printMessage(msg) {
-  var div = document.createElement('div');
-  div.innerHTML = msg;
-  document.getElementById('messages').appendChild(div);
+    var div = document.createElement('div');
+    div.innerHTML = msg;
+    document.getElementById('messages').appendChild(div);
 }
 
 /**
  * Funkcja czyszcząca wiadomości
  */
 function clearMessages() {
-  document.getElementById('messages').innerHTML = '';
+    document.getElementById('messages').innerHTML = '';
 }
 
 /**
  * Funkcja rozpoznaje ruch na podstawie ID.
  */
 function getMoveName(argMoveId) {
-  console.log('Wywołano funkcję getMoveName z argumentem: ' + argMoveId);
-  if (argMoveId == 1) {
-    return 'kamień';
-  } else if (argMoveId == 2) {
-    return 'papier';
-  } else if (argMoveId == 3) {
-    return 'nożyce';
-  } else {
-    printMessage('Nie znam ruchu o id ' + argMoveId + '. Zakładam, że chodziło o "kamień".');
-    return 'kamień'; // Zabezpieczenie w razie błędnego ruchu
-  }
+    console.log('Wywołano funkcję getMoveName z argumentem: ' + argMoveId);
+    if (argMoveId == 1) {
+        return 'kamień';
+    } else if (argMoveId == 2) {
+        return 'papier';
+    } else if (argMoveId == 3) {
+        return 'nożyce';
+    } else {
+        printMessage('Nie znam ruchu o id ' + argMoveId + '. Zakładam, że chodziło o "kamień".');
+        return 'kamień'; // Zabezpieczenie w razie błędnego ruchu
+    }
 }
 
 /**
  * Funkcja rozstrzyga wynik gry na podstawie ruchów gracza i komputera.
  */
 function displayResult(argPlayerMove, argComputerMove) {
-  console.log('Wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
+    console.log('Wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
 
-  if (
-    (argPlayerMove == 'papier' && argComputerMove == 'kamień') || 
-    (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') || 
-    (argPlayerMove == 'nożyce' && argComputerMove == 'papier')
-  ) {
-    printMessage('Wygrywasz!');
-  } else if (argPlayerMove == argComputerMove) {
-    printMessage('Remis!');
-  } else {
-    printMessage('Przegrywasz :(');
-  }
+    if (
+        (argPlayerMove == 'papier' && argComputerMove == 'kamień') || 
+        (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') || 
+        (argPlayerMove == 'nożyce' && argComputerMove == 'papier')
+    ) {
+        printMessage('Wygrywasz!');
+    } else if (argPlayerMove == argComputerMove) {
+        printMessage('Remis!');
+    } else {
+        printMessage('Przegrywasz :(');
+    }
 
-  printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+    printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 }
 
 // Przypisanie przycisków do zmiennych
@@ -87,20 +87,20 @@ buttonTest = document.getElementById('button-test');
 
 // Obsługa kliknięć przycisków
 buttonRock.addEventListener('click', function() {
-  buttonClicked('kamień');
+    buttonClicked('kamień');
 });
 
 buttonPaper.addEventListener('click', function() {
-  buttonClicked('papier');
+    buttonClicked('papier');
 });
 
 buttonScissors.addEventListener('click', function() {
-  buttonClicked('nożyce');
+    buttonClicked('nożyce');
 });
 
 // Obsługa guzika TEST
 buttonTest.addEventListener('click', function() {
-  clearMessages();
-  printMessage('Kliknięto guzik TEST! Możesz teraz kontynuować grę.');
-  console.log('Kliknięto guzik TEST.');
+    clearMessages();
+    printMessage('Kliknięto guzik TEST! Możesz teraz kontynuować grę.');
+    console.log('Kliknięto guzik TEST.');
 });
